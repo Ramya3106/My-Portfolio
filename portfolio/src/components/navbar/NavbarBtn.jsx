b0602449072191f9dec089cb2ef56b95821e5075
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-08 12:03:11 +0530

    updaded on navbarbtn, navbarlogo,navbarmain

--- portfolio/src/components/navbar/NavbarBtn.jsx
+++ portfolio/src/components/navbar/NavbarBtn.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
-  return <div>NavbarBtn</div>;
+  return (
+    <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:scale-110">
+      Hire Me
+      <LuArrowDownRight />
+    </button>
+  );
 };
 
 export default NavbarBtn;
--- portfolio/src/components/navbar/NavbarLogo.jsx
+++ portfolio/src/components/navbar/NavbarLogo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NavbarLogo = () => {
-  return <div>NavbarLogo</div>;
+  return (
+    <div>
+      <h1 className="text-white text-2xl">Ramya V</h1>
+    </div>
+  );
 };
 
 export default NavbarLogo;
