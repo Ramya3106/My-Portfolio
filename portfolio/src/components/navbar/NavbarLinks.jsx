5e8794f21913c998f88bb57e83ca2a38de4e3cdb
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-08 12:43:43 +0530

    updaded on navbarlink

--- portfolio/src/components/navbar/NavbarLinks.jsx
+++ portfolio/src/components/navbar/NavbarLinks.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+const links = [
+  { link: "About Me", section: "about" },
+  { link: "Skills", section: "skills" },
+  { link: "Experience", section: "experience" },
+  { link: "Projects", section: "projects" },
+  { link: "Contact", section: "contact" },
+];
+
 const NavbarLinks = () => {
-  return <div>NavbarLinks</div>;
+  return (
+    <ul>
+      {links.map((link, index) => (
+        <li key={index}>{link.link}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default NavbarLinks;
