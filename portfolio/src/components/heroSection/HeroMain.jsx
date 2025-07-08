0777aec8fb1d54b8ea9c4ab87c526716923793c6
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-08 19:49:39 +0530

    initialized herosection

---
+++ portfolio/src/components/heroSection/HeroMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroText from "./HeroText";
+import HeroPic from "./HeroPic";
+
+const HeroMain = () => {
+  return (
+    <div>
+      <HeroText />
+      <HeroPic />
+    </div>
+  );
+};
+
+export default HeroMain;
---
+++ portfolio/src/components/heroSection/HeroText.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
+      <h2 className="lg:text-2xl sm:text-xl uppercase text-cyan">
+        Full Stack Web Developer
+      </h2>
+      <h1 className="md:text-[2.8rem] lg:text-6xl sm:text-4xl font-bold font-special">
+        Ramya V
+      </h1>
+      <p>
+        Passionate full stack developer with a love for clean code, clever
+        design, and continuous learning.
+      </p>
+    </div>
+  );
+};
+
+export default HeroText;
