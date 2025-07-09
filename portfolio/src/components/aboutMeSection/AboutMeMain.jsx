e2b3c39b5bd65e21eddd00753f9c75a748d71bbc
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-09 20:32:35 +0530

    initialized aboutmemenu.jsx

--- portfolio/src/components/aboutMeSection/AboutMeMain.jsx
+++ portfolio/src/components/aboutMeSection/AboutMeMain.jsx
@@ -4,7 +4,7 @@ import AboutMeImage from "./AboutMeImage";
 
 const AboutMeMain = () => {
   return (
-    <div>
+    <div className="flex md:flex-row sm:flex-col gap-12 px-4 max-w-[1200px] mx-auto mt-[100px] justify-between items-center">
       <AboutMeText />
       <AboutMeImage />
     </div>
