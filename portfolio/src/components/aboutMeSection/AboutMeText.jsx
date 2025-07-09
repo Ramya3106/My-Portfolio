49e37fb7b6706c53109f403397d7949d61d36136
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-09 20:42:14 +0530

    updated aboutmetext.jsx

--- portfolio/src/components/aboutMeSection/AboutMeText.jsx
+++ portfolio/src/components/aboutMeSection/AboutMeText.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const AboutMeText = () => {
-  return <div>AboutMeText</div>;
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        I’m a passionate web developer who enjoys solving real-world problems
+        through clean, efficient code. With hands-on experience in real-time
+        projects, I bring practical solutions to complex challenges. Beyond
+        coding, I’m committed to continuous learning and staying updated with
+        the latest technologies. I also believe in sharing knowledge to inspire
+        and help others achieve their goals. Whether it’s building user-friendly
+        websites or collaborating on impactful tech solutions, I’m always
+        excited to grow and contribute.
+      </p>
+      <button>My Projects</button>
+    </div>
+  );
 };
 
 export default AboutMeText;
