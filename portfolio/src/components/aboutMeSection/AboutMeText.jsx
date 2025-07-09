e71445ebf7527c608aa023cb10471b64c780cdf6
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-09 20:52:56 +0530

    updated aboutmetext.jsx

--- portfolio/src/components/aboutMeSection/AboutMeText.jsx
+++ portfolio/src/components/aboutMeSection/AboutMeText.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const AboutMeText = () => {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>
+    <div className="flex felx-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan md-10">About Me</h2>
+      <p className="">
         I’m a passionate web developer who enjoys solving real-world problems
         through clean, efficient code. With hands-on experience in real-time
         projects, I bring practical solutions to complex challenges. Beyond
@@ -14,7 +14,9 @@ const AboutMeText = () => {
         websites or collaborating on impactful tech solutions, I’m always
         excited to grow and contribute.
       </p>
-      <button>My Projects</button>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">
+        My Projects
+      </button>
     </div>
   );
 };
