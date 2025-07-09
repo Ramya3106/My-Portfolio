7956f3a0e018e02519b0ca058040f179e50ca71c
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-09 19:43:54 +0530

    updated on app.jsx and heromain.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import HeroMain from "./components/heroSection/HeroMain";
 import NavbarMain from "./components/navbar/NavbarMain";
+import HeroGradient from "./components/heroSection/HeroGradient";
+import SubHeroSection from "./components/heroSection/SubHeroSection";
 
 function App() {
   return (
     <main className="font-body">
       <NavbarMain />
       <HeroMain />
+      <HeroGradient />
+      <SubHeroSection />
     </main>
   );
 }
---
+++ portfolio/src/components/heroSection/SubHeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SubHeroSection = () => {
+  return (
+    <div className="">
+      <p>Fast Learner</p>
+      <p>Team Work</p>
+      <p>Details Master</p>
+    </div>
+  );
+};
+
+export default SubHeroSection;
