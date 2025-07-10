b35c5daa9c90018705ed04fb9e048dac663ad886
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-10 13:24:28 +0530

    updated allskills.jsx

--- portfolio/src/components/skillsSection/AllSkills.jsx
+++ portfolio/src/components/skillsSection/AllSkills.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import SingleSkill from "./SingleSkill";
+
+const skills = [
+  {
+    skill: "HTML",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "CSS",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "Javascript",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "ReactJs",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "TailwindCSS",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "NodeJs",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "ExpressJS",
+    icon: "FaHtml5",
+  },
+  {
+    skill: "MangoDB",
+    icon: "FaHtml5",
+  },
+];
 
 const AllSkills = () => {
-  return <div>AllSkills</div>;
+  return (
+    <div className="flex items-center justify-center relative gap-2 max-w-[1200px] mx-auto">
+      {skills.map((item, index) => {
+        return (
+          <SingleSkill key={index} text={item.text} imgsvg={<item.icon />} />
+        );
+      })}
+    </div>
+  );
 };
 
 export default AllSkills;
--- portfolio/src/components/skillsSection/SkillsMain.jsx
+++ portfolio/src/components/skillsSection/SkillsMain.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SkillsText from "./SkillsText";
+import AllSkills from "./AllSkills";
 
 const SkillsMain = () => {
-  return <div>SkillsMain</div>;
+  return (
+    <div>
+      <SkillsText />
+      <AllSkills />
+    </div>
+  );
 };
 
 export default SkillsMain;
