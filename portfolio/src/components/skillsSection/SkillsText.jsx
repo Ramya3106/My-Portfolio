03e665c07d8ea9d7f6a9679dcfc6899b82ac69f0
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-11 12:49:12 +0530

    updated at singlemain.jsx

--- portfolio/src/components/skillsSection/SkillsText.jsx
+++ portfolio/src/components/skillsSection/SkillsText.jsx
@@ -4,7 +4,7 @@ const SkillsText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
       <h2 className="text-6xl text-cyan mb-10">My Skills</h2>
-      <p className="text-lh text-center text-white">
+      <p className="text-lg text-center text-white">
         I have hands-on experience with the skills shown below and have applied
         them in real-world projects. These technologies are not just known to me
         and I’ve built, debugged, and delivered using them. I continuously learn
