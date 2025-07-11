26abab16d4aaa82f2b10877f8b9eed2f6f897d54
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-11 12:14:51 +0530

    updated at singletext.jsx

--- portfolio/src/components/skillsSection/SkillsText.jsx
+++ portfolio/src/components/skillsSection/SkillsText.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const SkillsText = () => {
-  return <div>SkillsText</div>;
+  return (
+    <div className="flex flex-col items-center mt-[100px]">
+      <h2 className="text-6xl text-cyan mb-10">My Skills</h2>
+      <p className="text-lh text-center text-white">
+        I have hands-on experience with the skills shown below and have applied
+        them in real-world projects. These technologies are not just known to me
+        and I’ve built, debugged, and delivered using them. I continuously learn
+        and improve to stay updated and effective in what I do.
+      </p>
+    </div>
+  );
 };
 
 export default SkillsText;
