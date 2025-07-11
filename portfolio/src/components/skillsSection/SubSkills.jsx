e6a3b5b3883bb3c5f573e9c00f17c6a5c8c123ad
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-11 13:29:35 +0530

    updated at subskills.jsx

--- portfolio/src/components/skillsSection/SubSkills.jsx
+++ portfolio/src/components/skillsSection/SubSkills.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SubSkills = () => {
-  return <div>SubSkills</div>;
+  return (
+    <div className="border-y-2 border-x-lightGrey relative">
+      <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
+      <img src="../../public/images/subSkills.jpg" alt="subSkills Image" />
+    </div>
+  );
 };
 
 export default SubSkills;
