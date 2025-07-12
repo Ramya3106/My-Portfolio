03f19143f3cbbeff9ab2f6d914e9886449d79b62
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-12 19:32:45 +0530

    updated at experiencemain.jsx

--- portfolio/src/components/experienceSection/ExperienceMain.jsx
+++ portfolio/src/components/experienceSection/ExperienceMain.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ExperienceText from "./ExperienceText";
+import ExperienceTop from "./ExperienceTop";
+import AllExperience from "./AllExperience";
 
 const ExperienceMain = () => {
-  return <div>ExperienceMain</div>;
+  return (
+    <div>
+      <ExperienceText />
+      <ExperienceTop />
+      <AllExperience />
+    </div>
+  );
 };
 
 export default ExperienceMain;
