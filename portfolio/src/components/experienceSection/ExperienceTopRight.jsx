99af2568de37a83a9c22822020d249762e796026
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-14 17:16:49 +0530

    updated at experiencetopright.jsx

--- portfolio/src/components/experienceSection/ExperienceTopRight.jsx
+++ portfolio/src/components/experienceSection/ExperienceTopRight.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ExperienceTopRight = () => {
-  return <div>ExperienceTopRight</div>;
+  return (
+    <div className="xl:w-[25%] lg:w-[30%] border border-lightBrown px-4 rounded-xl">
+      <p>
+        I specialize in React and medern Javascript, leveraging best practice to
+        create scalable and maintainable solutions. My experience spans working
+        on diverse projects, from small business websites to complex front-end
+        and back-end systems, always aiming for clean code andexceptional user
+        experience.
+      </p>
+    </div>
+  );
 };
 
 export default ExperienceTopRight;
