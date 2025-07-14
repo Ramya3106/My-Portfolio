b485c51d3c3233cd5be7b5feb59cb459199341e5
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-14 23:52:08 +0530

    updated at projectmain.jsx

--- portfolio/src/components/projectsSection/ProjectMain.jsx
+++ portfolio/src/components/projectsSection/ProjectMain.jsx
@@ -2,6 +2,58 @@ import React from "react";
 import ProjectText from "./ProjectText";
 import SingleProject from "./SingleProject";
 
+const projects = [
+  {
+    name: "Travel",
+    year: "Nov2023",
+    align: "right",
+    image: "../../publicc/images/project-img6.png",
+    link: "#",
+  },
+  {
+    name: "Ice Parlour",
+    year: "jan2024",
+    align: "left",
+    image: "../../public/images/project-img10.png",
+    link: "#",
+  },
+  {
+    name: "Coffee Shop",
+    year: "May2024",
+    align: "right",
+    image: "../../public/images/project-img12.png",
+    link: "",
+  },
+  {
+    name: "",
+    year: "",
+    align: "",
+    image: "",
+    link: "",
+  },
+  {
+    name: "",
+    year: "",
+    align: "",
+    image: "",
+    link: "",
+  },
+  {
+    name: "",
+    year: "",
+    align: "",
+    image: "",
+    link: "",
+  },
+  {
+    name: "",
+    year: "",
+    align: "",
+    image: "",
+    link: "",
+  },
+];
+
 const ProjectMain = () => {
   return (
     <div>
