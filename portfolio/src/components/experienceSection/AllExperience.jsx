3374fd68b99fd2f5188edb0523af4dff5af51eea
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-14 18:01:13 +0530

    updated at allexperience.jsx

--- portfolio/src/components/experienceSection/AllExperience.jsx
+++ portfolio/src/components/experienceSection/AllExperience.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
+const experiences = [
+  {
+    internship: "Front-End Developer",
+    company: "Griffendor",
+    date: 2022,
+    responsibilities: [
+      "Participating in large scale application",
+      "Working on the performance of web applications",
+      "Generating new ideas for better user experience",
+    ],
+  },
+  {
+    internship: "Front-End Developer",
+    company: "System Tron",
+    date: 2022,
+    responsibilities: [
+      "Participating in large scale application",
+      "Working on the performance of web applications",
+      "Generating new ideas for better user experience",
+    ],
+  },
+  {
+    internship: "Front-End Developer",
+    company: "Oasis Infobyte",
+    date: 2022,
+    responsibilities: [
+      "Participating in large scale application",
+      "Working on the performance of web applications",
+      "Generating new ideas for better user experience",
+    ],
+  },
+];
 const AllExperience = () => {
   return <div>AllExperience</div>;
 };
