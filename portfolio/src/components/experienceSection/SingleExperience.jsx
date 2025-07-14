cd302ef38145ac5ae829317a3e8d923812fe8e6e
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-14 21:23:40 +0530

    updated at singleexperience.jsx

--- portfolio/src/components/experienceSection/SingleExperience.jsx
+++ portfolio/src/components/experienceSection/SingleExperience.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const SingleExperience = ({ experience }) => {
   return (
-    <div>
-      <p>{experience.job}</p>
-      <p>{experience.company}</p>
-      <p>{experience.date}</p>
-      <ul>
+    <div className="md:h-[350px] md:w-[240px] sm:h-auto sm:w-auto border-2 border-orange border-dashed rounded-2xl mt-12 p-4">
+      <p className="font-bold text-cyan">{experience.job}</p>
+      <p className="text-orange">{experience.company}</p>
+      <p className="text-lightGrey">{experience.date}</p>
+      <ul className="list-disc mt-4 pl-4 text-white">
         {experience.responsibilities.map((resp, index) => {
           return <li key={index}>{resp}</li>;
         })}
