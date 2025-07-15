af0fa14947bd05e300b0806892f81347414c9485
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 13:29:19 +0530

    updated at contactsocial.jsx

--- portfolio/src/components/contactSection/ContactSocial.jsx
+++ portfolio/src/components/contactSection/ContactSocial.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const ContactSocial = () => {
-  return <div>ContactSocial</div>;
+const ContactSocial = ({ Icon, link }) => {
+  return (
+    <div className="text-2xl h-12 w-12 border border-orange text-orange rounded-full p-3 flex items-center justify-center">
+      <a href={link} className="cursor-pointer">
+        <Icon />
+      </a>
+    </div>
+  );
 };
 
 export default ContactSocial;
