00eee5d53b16e336a1adee6157e9d8f9af11b2be
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 12:46:10 +0530

    updated at contactright.jsx

--- portfolio/src/components/contactSection/ContactMeRight.jsx
+++ portfolio/src/components/contactSection/ContactMeRight.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ContactInfo from "./ContactInfo";
+import ContactSocial from "./ContactSocial";
 
 const ContactMeRight = () => {
-  return <div>ContactMeRight</div>;
+  return (
+    <div>
+      <img
+        src="../../public/images/email-img.jpg"
+        alt="Contact Me"
+        className="max-w-[300px]"
+      />
+      <ContactInfo />
+      <ContactSocial />
+    </div>
+  );
 };
 
 export default ContactMeRight;
