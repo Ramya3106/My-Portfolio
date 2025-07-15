a3c2b1ea4e80fe4ca380b127f7287fdc46efe300
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 13:52:14 +0530

    updated at contactform.jsx

--- portfolio/src/components/contactSection/ContactForm.jsx
+++ portfolio/src/components/contactSection/ContactForm.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <div>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Your Name"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <input
+          type="email"
+          placeholder="Your Email"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <textarea
+          type="text"
+          placeholder="Message"
+          rows="9"
+          cols="50"
+          required
+        />
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
