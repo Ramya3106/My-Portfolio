fa2b34fb39774674dcdc669e8553a5fbe35eab06
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 13:44:22 +0530

    updated at contactmeright.jsx

--- portfolio/src/components/contactSection/ContactMeRight.jsx
+++ portfolio/src/components/contactSection/ContactMeRight.jsx
@@ -4,7 +4,7 @@ import ContactSocial from "./ContactSocial";
 
 const ContactMeRight = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center gap-12">
       <img
         src="../../public/images/email-img.jpg"
         alt="Contact Me"
--- portfolio/src/components/contactSection/ContactSocial.jsx
+++ portfolio/src/components/contactSection/ContactSocial.jsx
@@ -6,7 +6,7 @@ import { FaTwitter } from "react-icons/fa";
 
 const ContactSocial = () => {
   return (
-    <div className="flex gap-4 my-8">
+    <div className="flex gap-4">
       <SingleContactSocial link="#" Icon={FiGithub} />
       <SingleContactSocial link="#" Icon={FaLinkedinIn} />
       <SingleContactSocial link="#" Icon={FaTwitter} />
