70f4eb15fa08a10697c638b0a6e28c4c4ec1f55f
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 12:50:27 +0530

    updated at contactsocial.jsx

--- portfolio/src/components/contactSection/ContactSocial.jsx
+++ portfolio/src/components/contactSection/ContactSocial.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const ContactSocial = () => {
-  return <div>ContactSocial</div>;
+const ContactSocial = ({ text, Image }) => {
+  return (
+    <div className="flex gap-4 items-center justify-start">
+      <image className="text-3xl" />
+      <p>{text}</p>
+    </div>
+  );
 };
 
 export default ContactSocial;
