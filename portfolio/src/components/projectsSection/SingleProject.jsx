ca141b2f8a670ade7f04be841b4c954889aa141b
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-15 11:18:45 +0530

    updated at singleproject.jsx

--- portfolio/src/components/projectsSection/SingleProject.jsx
+++ portfolio/src/components/projectsSection/SingleProject.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BiSolidRightTopArrowCircle } from "react-icons/bi";
 
-const SingleProject = () => {
-  return <div>SingleProject</div>;
+const SingleProject = ({ name, year, align, image, link }) => {
+  return (
+    <div>
+      <div>
+        <h2>{name}</h2>
+        <h2>{year}</h2>
+        <a>
+          view <BiSolidRightTopArrowCircle />
+        </a>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default SingleProject;
