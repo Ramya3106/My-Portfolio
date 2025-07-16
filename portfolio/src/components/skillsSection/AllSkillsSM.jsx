9458c26e2ebfb1dd9a79423d6efb6004b7651a39
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-16 12:16:45 +0530

    updated at skillsection

--- portfolio/src/components/skillsSection/AllSkillsSM.jsx
+++ portfolio/src/components/skillsSection/AllSkillsSM.jsx
@@ -6,6 +6,9 @@ import { FaReact } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { IoLogoNodejs } from "react-icons/io5";
 import { SiMongodb } from "react-icons/si";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const skills = [
   {
@@ -47,10 +50,17 @@ const AllSkillsSM = () => {
     <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-12 my-12">
       {skills.map((item, index) => {
         return (
-          <div key={index} className="flex flex-col items-center">
+          <motion.div
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }}
+            key={index}
+            className="flex flex-col items-center"
+          >
             <item.icon className="text-7xl text-orange" />
             <p className="text-center mt-4 text-white">{item.skill}</p>
-          </div>
+          </motion.div>
         );
       })}
     </div>
