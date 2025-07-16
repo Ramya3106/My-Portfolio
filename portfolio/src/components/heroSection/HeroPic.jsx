d05f43ac32ca4e0d0f5dd54187efd9eb51c1c97e
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-16 11:54:08 +0530

    updated at heropic.jsx

--- portfolio/src/components/heroSection/HeroPic.jsx
+++ portfolio/src/components/heroSection/HeroPic.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { PiHexagonThin } from "react-icons/pi";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const HeroPic = () => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <motion.div
+      variants={fadeIn("left", 0.2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0 }}
+      className="h-full flex items-center justify-center"
+    >
       <img
         src="../../public/images/HeroPic.png"
         alt="Ramya V"
@@ -12,7 +21,7 @@ const HeroPic = () => {
       <div className="absolute -z-10 flex justify-center items-center animate-pulse">
         <PiHexagonThin className="md:h-[90%] sm:h-[120%] md:min-h-[450px] lg:min-h-[550px] sm:min-h-[550px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
