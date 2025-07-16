18839fb3ae6653fe88bdfb2463898751b59afb17
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-16 12:13:04 +0530

    updated at skillsection

--- portfolio/src/components/skillsSection/AllSkills.jsx
+++ portfolio/src/components/skillsSection/AllSkills.jsx
@@ -7,6 +7,9 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { IoLogoNodejs } from "react-icons/io5";
 import { SiMongodb } from "react-icons/si";
 import SingleSkill from "./SingleSkill";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const skills = [
   {
@@ -45,12 +48,25 @@ const skills = [
 
 const AllSkills = () => {
   return (
-    <div className="flex items-center justify-center relative gap-2 max-w-[1200px] mx-auto">
-      {skills.map((item, index) => {
-        return (
-          <SingleSkill key={index} text={item.skill} imgsvg={<item.icon />} />
-        );
-      })}
+    <div>
+      <div className="flex items-center justify-center relative gap-2 max-w-[1200px] mx-auto">
+        {skills.map((item, index) => {
+          return (
+            <motion.div
+              variants={fadeIn("up", `0.${index}`)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0 }}
+            >
+              <SingleSkill
+                key={index}
+                text={item.skill}
+                imgsvg={<item.icon />}
+              />
+            </motion.div>
+          );
+        })}
+      </div>
     </div>
   );
 };
