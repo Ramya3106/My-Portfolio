c82871f0823657022439983d9e2124a776213dbd
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-17 19:35:41 +0530

    updated at contactmain.jsx

--- portfolio/src/components/contactSection/ContactMain.jsx
+++ portfolio/src/components/contactSection/ContactMain.jsx
@@ -6,7 +6,7 @@ const ContactMain = () => {
   return (
     <div
       id="contact"
-      className="max-w-[1200px] mx-auto items-center justify-center mt-[100px] px-4"
+      className="max-w-[1200px] mx-auto items-center justify-center mt-[100px] px-4 py-10"
     >
       <h2 className="text-6xl text-cyan mb-10 text-center">Contact Me</h2>
       <div className="flex justify-between gap-24 bg-brown p-8 rouned-2xl lg:flex-row sm:flex-col">
